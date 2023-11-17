d02474f771bf8fa095966b8fe4c37788a91167e8
Chandramauli <chandramauliguptach@gmail.com>
2023-11-17 17:18:11 +0530

    parsed xml response to generate key array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import axios from 'axios'
 import './App.css'
+import { useEffect } from 'react'
+import { useState } from 'react';
 
 function App() {
 
+  const[data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://commfind.s3.ap-south-1.amazonaws.com/?list-type=2")
+      .then((response) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(response.data, "application/xml");
+        const namespace = "http://s3.amazonaws.com/doc/2006-03-01/";
+        const keys = xmlDoc.getElementsByTagNameNS(namespace, "Key");
+
+        // Convert NodeList to array
+        const keyArray = Array.from(keys).map(keyElement => keyElement.textContent);
+
+        setData(keyArray);
+        console.log(keyArray);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+
   return (
     <>
-       <img src = {`https://commfind.s3.ap-south-1.amazonaws.com/cfa3.png`}></img>
+        
     </>
   )
 }
