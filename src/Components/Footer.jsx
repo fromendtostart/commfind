cec058c2cf2e79f71b5fdfa1924be5aa2ab33f36
Chandramauli <chandramauliguptach@gmail.com>
2023-11-23 16:21:31 +0530

    added community note

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,18 @@
 function Footer(){
   return (
     <footer className="bg-gray-900 text-white p-6 fixed bottom-0 w-full">
-    <div className="container mx-auto flex justify-between items-center">
-      <div>
-        <h3 className="text-lg font-semibold mb-2">Contact Information</h3>
-        <p className="text-gray-500"><a href= "https://in.linkedin.com/in/chandramaulig" target="_blank" rel="noreferrer">LinkedIn</a></p>
-        <p className="text-gray-500"><a href= "https://twitter.com/NervesOfSilicon" target="_blank" rel="noreferrer">Twitter</a></p>
-        <p className="text-gray-500"><a href= "mailto:chandramauliguptach@gmail.com" target="_blank" rel="noreferrer">Email</a></p>
+      <div className="mx-auto flex justify-between items-center px-12">
+        <div className="contacts">
+          <h3 className="text-lg font-semibold mb-2">Contact Information</h3>
+          <p className="text-gray-500"><a href= "https://in.linkedin.com/in/chandramaulig" target="_blank" rel="noreferrer">LinkedIn</a></p>
+          <p className="text-gray-500"><a href= "https://twitter.com/NervesOfSilicon" target="_blank" rel="noreferrer">Twitter</a></p>
+          <p className="text-gray-500"><a href= "mailto:chandramauliguptach@gmail.com" target="_blank" rel="noreferrer">Email</a></p>
+        </div>
+        <div className="note">
+          <p>You can collect all items from BHS Room nearby hostel exit</p>
       </div>
-    </div>
+      </div>
+      
   </footer>
   );
 }
